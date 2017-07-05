3daabace497384d7d280f79d072ed04f53015007
Stephanie Lozier <Stephlozier@gmail.com>
2017-07-05 18:20:32 +1000

    added JSON encoding

--- vulnParser.go
+++ vulnParser.go
@@ -14,13 +14,12 @@ type Vuln struct {
 	Date     string `json:"date_reported"`
 }
 
-//ParseJSON returns the JSON in the given file
+//ParseJSON returns the JSON in the file found at the given path
 func ParseJSON(fileName string) *[]Vuln {
 
 	dataFile, err := os.Open(fileName)
 	if err != nil {
 		log.Fatal(err)
-
 	}
 
 	jsonParser := json.NewDecoder(dataFile)
@@ -29,4 +28,13 @@ func ParseJSON(fileName string) *[]Vuln {
 		log.Fatal(err)
 	}
 	return json
+}
+
+//EncodeVulns returns an array of Vulns as JSON
+func EncodeVulns(vulns *[]Vuln) []byte {
+	b, err := json.Marshal(vulns)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return b
 }
