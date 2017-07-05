eb2c7135a45c5c7a669607a6f33a2444e6e8d106
Stephanie Lozier <Stephlozier@gmail.com>
2017-07-05 23:33:04 +1000

    renaming file + RMI func

--- jsonVulnUtil.go
+++ jsonVulnUtil.go
@@ -1,4 +1,4 @@
-package jsonHandler
+package jsonVulnUtil
 
 import (
 	"encoding/json"
@@ -27,14 +27,28 @@ func ParseJSON(fileName string) *[]Vuln {
 	if err = jsonParser.Decode(json); err != nil {
 		log.Fatal(err)
 	}
+
 	return json
 }
 
 //EncodeVulns returns an array of Vulns as JSON
 func EncodeVulns(vulns *[]Vuln) []byte {
+
 	b, err := json.Marshal(vulns)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	return b
+}
+
+//RemoveMalformedInput removes structs will nil values from JSON conversion
+func RemoveMalformedInput(vulns *[]Vuln) *[]Vuln {
+	sanitised := new([]Vuln)
+	for _, value := range *vulns {
+		if value.ID != 0 && value.Severity != 0 && value.Title != "" && value.Date != "" {
+			*sanitised = append(*sanitised, value)
+		}
+	}
+	return sanitised
 }
