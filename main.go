6e94649b1d0da7d291ef672413342d864512cafb
Stephanie Lozier <Stephlozier@gmail.com>
2017-07-07 03:15:30 +1000

    implemented filterHandler

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"net/http"
+	"strconv"
 
+	"github.com/xalian/ugchallenge/filters"
 	"github.com/xalian/ugchallenge/jsonvuln"
 )
 
@@ -18,8 +21,27 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { filterHandler(w, r, vulns) })
 	http.ListenAndServe(*portPtr, nil)
 }
+
 func filterHandler(w http.ResponseWriter, r *http.Request, vulns []jsonvuln.Vuln) {
 	limit := r.URL.Query().Get("limit")
 	minSeverity := r.URL.Query().Get("severity_at_least")
 	since := r.URL.Query().Get("since")
+
+	if minSeverity != "" {
+		sev, err := strconv.Atoi(minSeverity)
+		if err == nil {
+			vulns = filters.FilterSeverity(sev, vulns)
+		}
+	}
+	if since != "" {
+		//TODO: check valid date format
+		vulns = filters.FilterDate(since, vulns)
+	}
+	if limit != "" {
+		lmt, err := strconv.Atoi(limit)
+		if err == nil {
+			vulns = filters.FilterLimit(lmt, vulns)
+		}
+	}
+	json.NewEncoder(w).Encode(vulns)
 }
