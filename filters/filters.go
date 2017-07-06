9994bb19c360d0df2055a5932d4af0d32cfd1fd7
Stephanie Lozier <Stephlozier@gmail.com>
2017-07-07 02:09:14 +1000

    filterDate

--- filters/filters.go
+++ filters/filters.go
@@ -1,12 +1,25 @@
 package filters
 
 import (
+	"strconv"
+	"strings"
+
 	"github.com/xalian/ugchallenge/jsonvuln"
 )
 
-func filterLimit(limit int, vulns []jsonvuln.Vuln) []jsonvuln.Vuln {
+func FilterLimit(limit int, vulns []jsonvuln.Vuln) []jsonvuln.Vuln {
 	limit = max(limit, len(vulns))
 	return vulns[0 : limit-1]
+}
+
+func FilterDate(limitDate string, vulns []jsonvuln.Vuln) []jsonvuln.Vuln {
+	filtered := new([]jsonvuln.Vuln)
+	for _, value := range vulns {
+		if dateSince(value.Date, limitDate) {
+			*filtered = append(*filtered, value)
+		}
+	}
+	return *filtered
 }
 
 func max(a, b int) int {
@@ -14,4 +27,39 @@ func max(a, b int) int {
 		return a
 	}
 	return b
+}
+
+//dateSince checks if date is later than limit
+func dateSince(date, limit string) bool {
+
+	dateParts := strings.Split(date, "-")
+	limitParts := strings.Split(limit, "-")
+
+	dateYear, _ := strconv.Atoi(dateParts[0])
+	limitYear, _ := strconv.Atoi(limitParts[0])
+	switch {
+	case dateYear > limitYear:
+		return true
+	case dateYear < limitYear:
+		return false
+	}
+
+	dateMonth, _ := strconv.Atoi(dateParts[1])
+	limitMonth, _ := strconv.Atoi(limitParts[1])
+	switch {
+	case dateMonth > limitMonth:
+		return true
+	case dateMonth < limitMonth:
+		return false
+	}
+
+	dateDay, _ := strconv.Atoi(dateParts[2])
+	limitDay, _ := strconv.Atoi(limitParts[2])
+	switch {
+	case dateDay > limitDay:
+		return true
+	case dateDay < limitDay:
+		return false
+	}
+	return false
 }
